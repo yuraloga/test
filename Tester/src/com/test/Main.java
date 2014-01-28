4eecdb5724008ad7fe5348553672a81c3196f62c
Yura Logazyak <yura.logazyak@gmail.com>
2014-01-28 21:55:56 +0200

    second user #0

---
+++ Tester/src/com/test/Main.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Tester t = new Tester();
+		t.setA(1);
+		
+		System.out.println(t.getA());
+	}
+
+}
---
+++ Tester/src/com/test/Tester.java
@@ -0,0 +1,32 @@
+package com.test;
+
+public class Tester {
+	private int a = 0;
+	private int b = 0;
+	private int c = 0;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public int getC() {
+		return c;
+	}
+
+	public void setC(int c) {
+		this.c = c;
+	}
+
+}
